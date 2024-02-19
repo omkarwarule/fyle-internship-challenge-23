6b1f20feeee4415aecb21a0b6167864479a1d1a1
Omkar_Warule <129866605+omkarwarule@users.noreply.github.com>
2024-02-19 13:04:23 +0530

    Update api.service.spec.ts

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
-
+ 
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
-
+  afterEach(() => {
+    httpMock.verify();
+  });
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
