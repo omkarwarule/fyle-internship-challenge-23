b33c01ee5f89cab942db23d3f5a706892c230ac1
Omkar_Warule <129866605+omkarwarule@users.noreply.github.com>
2024-02-19 13:04:00 +0530

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+ private apiUrl = 'https://api.github.com/users/'
   constructor(
     private httpClient: HttpClient
   ) { }
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(githubUsername: string): Observable<any> {
+    return this.httpClient.get<any>(`${this.apiUrl}${githubUsername}`).pipe(
+      map(response => {
+        // Convert the response to JSON format
+        console.log(response);
+        return JSON.parse(JSON.stringify(response));
+      })
+    );
   }
-
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getPublicRepositories(username: string): Observable<any> {
+    return this.httpClient.get<any>(`${this.apiUrl}${username}/repos`).pipe(
+      map(response=>{
+        console.log(response);
+        return JSON.parse(JSON.stringify(response));
+      })
+    );
+  }
 }
