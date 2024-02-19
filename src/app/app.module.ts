7ee09911ff6bdf39ec7aa4f396b1e2b7e287b419
Omkar_Warule <129866605+omkarwarule@users.noreply.github.com>
2024-02-19 13:05:55 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from  '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ResponseDirective } from './response.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ResponseDirective,
+    
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatTabsModule,
+    AlertModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(),
+    ApiService,
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
